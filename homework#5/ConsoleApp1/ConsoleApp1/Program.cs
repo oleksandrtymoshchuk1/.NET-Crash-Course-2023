50b1848b1b2aeac9c579f6ea5106c573fb838668
oleksandrtymoshchuk1 <124050132+oleksandrtymoshchuk1@users.noreply.github.com>
2023-02-23 16:13:32 +0200

    Add files via upload

---
+++ homework#5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+
+class Weapon
+{
+    enum Calibr {small, medium, large }
+    string model;
+    float range;
+    Calibr calibr;
+    int number;
+    int maxnumber;
+    bool isPowerON;
+
+    public void Initializer (string m, float r, int v)
+    {
+        model = m;
+        range = 0;
+        number = 0;
+        maxnumber = 0;
+        isPowerON = false;
+        calibr = Calibr.small;
+
+    }
+    public void ShowinfoGun()
+    {
+        Console.WriteLine($"Model: {model} range {range} number {number} maxnumber {maxnumber}\n" +
+            $"Power status {(isPowerON == true? "ON" : "OFF")}");
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Weapon my = new Weapon();
+        my.Initializer("Stugna", 7,62);
+        Weapon your = new Weapon();
+        my.ShowinfoGun();
+        
+    }
+}
